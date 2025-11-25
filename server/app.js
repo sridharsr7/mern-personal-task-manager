fb90e4bb0fdcfc5d36c5160abe5c939a7196cf98
sridharsr7 <sridhar9677a@gmail.com>
2025-11-25 13:49:29 +0530

    Fix duplicate cors middleware in app.js
    Removed duplicate cors middleware usage.

--- server/app.js
+++ server/app.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
